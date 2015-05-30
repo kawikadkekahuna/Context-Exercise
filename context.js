16f23c0e651ff9e26b9b165787f57fd77fa5e0f3
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-05-30 00:02:33 -1000

    context practice

--- context.js
+++ context.js
@@ -1,12 +1,16 @@
 var Robot = function (name) {
   this.name = name;
 
-}
+};
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
   return this.name + " can count to " + (op1 + op2);
 }
+
+
+
+
 
 var voltron = new Robot("Voltron");
 var bender = new Robot("Bender Rodriguez");
@@ -15,16 +19,17 @@ var megaman = new Robot("Mega Man");
 var bmo = new Robot("B-Mo");
 var wall_e = new Robot("Wall-E");
 
+var calculate = add.bind(bender);
 
+
+console.log( add(0,1) );
 // #1
-console.log(  );
+console.log( add.call(voltron,0,1) );
 
 // #2  
-console.log(  );
+console.log( add.apply(optimus,[20,30]) );
 
 // #3
-console.log(  );
+console.log( calculate('drinking','beer') );
 
 // #4
-
-console.log(  );
